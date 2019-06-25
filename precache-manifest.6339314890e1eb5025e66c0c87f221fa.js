54bbfaeebf65c9df2b45a2d1e0aed42e1046886b
bombs0817 <paul.l@tc-gaming.com>
2019-06-25 11:48:55 +0800

    Updates

---
+++ precache-manifest.6339314890e1eb5025e66c0c87f221fa.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d09e6f01a27db8e8b840f38a2d75698",
+    "url": "/react-shoppingCart/index.html"
+  },
+  {
+    "revision": "2186033e7e53cc76a07b",
+    "url": "/react-shoppingCart/static/css/main.2a5257a2.chunk.css"
+  },
+  {
+    "revision": "65607a495e4351203f39",
+    "url": "/react-shoppingCart/static/js/2.92d9e5ca.chunk.js"
+  },
+  {
+    "revision": "2186033e7e53cc76a07b",
+    "url": "/react-shoppingCart/static/js/main.98a65d50.chunk.js"
+  },
+  {
+    "revision": "679658bc04d33397edf7",
+    "url": "/react-shoppingCart/static/js/runtime~main.e98aa8e0.js"
+  },
+  {
+    "revision": "bd4817f0f45c972d21597cd5f1be60c5",
+    "url": "/react-shoppingCart/static/media/logo.bd4817f0.png"
+  },
+  {
+    "revision": "5a9d8c554c4ed155d2516341844c4825",
+    "url": "/react-shoppingCart/static/media/product1.5a9d8c55.jpg"
+  },
+  {
+    "revision": "913b75507fc0c19be577a15db8c4cfab",
+    "url": "/react-shoppingCart/static/media/product10.913b7550.jpg"
+  },
+  {
+    "revision": "c33e6f89fbe51db5c71d050a113d7d91",
+    "url": "/react-shoppingCart/static/media/product11.c33e6f89.jpg"
+  },
+  {
+    "revision": "93d4ebacda25b85676c2bfd8a4147547",
+    "url": "/react-shoppingCart/static/media/product2.93d4ebac.jpg"
+  },
+  {
+    "revision": "31cac9aef9bd19f98741bf72e67ef00f",
+    "url": "/react-shoppingCart/static/media/product3.31cac9ae.jpg"
+  },
+  {
+    "revision": "fc8d941074c08b43b7fa1dd6d7b50996",
+    "url": "/react-shoppingCart/static/media/product4.fc8d9410.jpg"
+  },
+  {
+    "revision": "f5c3d7b4d55382bf05fad470a797d29b",
+    "url": "/react-shoppingCart/static/media/product5.f5c3d7b4.jpg"
+  },
+  {
+    "revision": "6f8da7e07a088cec12a361dcfe0359c3",
+    "url": "/react-shoppingCart/static/media/product6.6f8da7e0.jpg"
+  },
+  {
+    "revision": "18f320f40992d297348934ecb16b97b6",
+    "url": "/react-shoppingCart/static/media/product7.18f320f4.jpg"
+  },
+  {
+    "revision": "0ad313beb0139b48ec4691ef73e1130d",
+    "url": "/react-shoppingCart/static/media/product8.0ad313be.jpg"
+  },
+  {
+    "revision": "0b76925f35274a9b85f9f6e0fd357d83",
+    "url": "/react-shoppingCart/static/media/product9.0b76925f.jpg"
+  }
+]);
